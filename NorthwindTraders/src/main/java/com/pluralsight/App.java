5e5cc14e1ad14aabe414cfffa3f8bf5d23b84987
Brenda Vang <vangsbrenda@gmail.com>
2025-06-11 12:26:48 -0500

    Removed if/else statements and replaced with switch statements. also added in methods

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -2,15 +2,19 @@ package com.pluralsight;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        // initializing scanner for user input
-        Scanner theScanner = new Scanner(System.in);
+    // initializing scanner for user input
+    private static Scanner theScanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         //making sure we passed in 2 arguments from the command line when we run the app
         //this is done with the app configuration in intellij (page 45 of the wb)
@@ -22,18 +26,13 @@ public class App {
             System.exit(1);
         }
 
-        // get the user name and password from the command line args
-        String username = args[0];
-        String password = args[1];
-
         // creating the datasource
         BasicDataSource dataSource = new BasicDataSource();
 
         // configure the datasource
         dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-
+        dataSource.setUsername(args[0]);
+        dataSource.setPassword(args[1]);
 
         // try with resources ensures connection is automatically closed at end of block
         // establishing connection to northwind database with username and password
@@ -45,6 +44,7 @@ public class App {
             // while loop so user can put in correct input/choice only
             while (running) {
 
+                // header and menu
                 System.out.println("\n          Hello •ᴗ•");
                 System.out.println("────────────୨ৎ────────────");
                 System.out.println("What would you like to do?");
@@ -57,161 +57,193 @@ public class App {
                 // reading user input, creating int choice variable, and going to next line
                 int choice = theScanner.nextInt();
 
-                // if user chooses 1, display this
-                if (choice == 1) {
-                    // try with resources ensures the prepared statement is automatically closed after use
-                    // execute SQL statement
-                    try (PreparedStatement preparedStatement = connection.prepareStatement(
-                            "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products"
-                    );
-                    // execute the query and getting results, try with resources ensures result set is automatically closed after use
-                    ResultSet resultSet = preparedStatement.executeQuery()
-                    ) {
 
-                        // header for products list
-                        System.out.println("\n            Products: ");
-                        System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
+                // now using switch statements for menu options and to call in methods
+                switch (choice) {
+                    case 1:
+                        // displays all products
+                        displayAllProducts(connection);
+                        break;
+                    case 2:
+                        // displays all customers
+                        displayAllCustomers(connection);
+                        break;
+                    case 3:
+                        // displays all categories first
+                        displayAllCategories(connection);
+                        // then displays question to ask user which category ID they'd like to choose to view all products in that category
+                        displayProductsByCategory(connection, theScanner);
+                        break;
+                    case 0:
+                        // if user chooses 0, exits the system
+                        System.out.println("────────────୨ৎ────────────");
+                        System.out.println("Exiting system now. Goodbye! •ᴗ•");
+                        running = false;
+                        break;
+                    default:
+                        // if user does not choose a valid option, display this message
+                        System.out.println("Invalid option, please choose between options 1, 2, 3, or 0.");
+                }
+            }
+        }
+    }
 
-                        // loop through the results
-                        while (resultSet.next()) {
-                            // process the data
-                            System.out.printf(
-                                    "Product ID:    %d\nName:          %s\nPrice:         %.2f\nStock:         %d\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
-                                    resultSet.getInt("ProductID"),
-                                    resultSet.getString("ProductName"),
-                                    resultSet.getDouble("UnitPrice"),
-                                    resultSet.getInt("UnitsInStock")
-                            );
-                        }
-                    }
-                    // if user chooses 2, display this
-                } else if (choice == 2) {
-                    try (PreparedStatement preparedStatement = connection.prepareStatement(
-                            "SELECT ContactName, CompanyName, City, Country, Phone FROM Customers"
-                    );
-                    // execute query
-                    ResultSet resultSet = preparedStatement.executeQuery()) {
+    // creating methods to interact w/ switch statements
+    private static void displayAllProducts(Connection connection) {
 
-                        // header for customers list
-                        System.out.println("\n            Customers: ");
-                        System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
+        // try with resources ensures the prepared statement is automatically closed after use
+        // execute SQL statement
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products"
+        );
+             // execute the query and getting results, try with resources ensures result set is automatically closed after use
+             ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
 
-                        // loop through the results
-                        while (resultSet.next()) {
-                            // process the data
-                            System.out.printf(
-                                    "Contact Name:     %s\nCompany Name:     %s\nCity:             %s\nCountry:          %s\nPhone:            %s\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
-                                    resultSet.getString("ContactName"),
-                                    resultSet.getString("CompanyName"),
-                                    resultSet.getString("City"),
-                                    resultSet.getString("Country"),
-                                    resultSet.getString("Phone")
-                            );
-                        }
+            // header for products list
+            System.out.println("\n            Products: ");
+            System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
 
-                    }
+            // loop through the results
+            while (resultSet.next()) {
+                // process the data
+                System.out.printf(
+                        "Product ID:    %d\nName:          %s\nPrice:         %.2f\nStock:         %d\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
+                        resultSet.getInt("ProductID"),
+                        resultSet.getString("ProductName"),
+                        resultSet.getDouble("UnitPrice"),
+                        resultSet.getInt("UnitsInStock")
+                );
+            }
 
-                    // if user chooses option 3, display categories
-                } else if (choice == 3) {
-                    // try with resources ensures the prepared statement is automatically closed after use
-                    try (PreparedStatement preparedStatement = connection.prepareStatement(
-                            "SELECT CategoryID, CategoryName FROM Categories"
-                    );
-                    // execute query
-                    ResultSet resultSet = preparedStatement.executeQuery()
-                    ) {
-                        // header for categories list
-                        System.out.println("\n            Categories: ");
-                        System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                        // loop through the results
-                        while (resultSet.next()) {
-                            // process the data
-                            System.out.printf(
-                                    "Category ID:       %s\nCategory Name:     %s\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
-                                    resultSet.getInt("CategoryID"),
-                                    resultSet.getString("CategoryName")
-                            );
+    private static void displayAllCustomers(Connection connection){
 
-                        }
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT ContactName, CompanyName, City, Country, Phone FROM Customers"
+        );
+             // execute query
+             ResultSet resultSet = preparedStatement.executeQuery()) {
 
-                    }
+            // header for customers list
+            System.out.println("\n            Customers: ");
+            System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
 
-                    // initializing categoryChoice outside of loop
-                    // using -1 as default vale to indicate it hasn't been assigned a valid category ID yet
-                    int categoryChoice = -1;
+            // loop through the results
+            while (resultSet.next()) {
+                // process the data
+                System.out.printf(
+                        "Contact Name:     %s\nCompany Name:     %s\nCity:             %s\nCountry:          %s\nPhone:            %s\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
+                        resultSet.getString("ContactName"),
+                        resultSet.getString("CompanyName"),
+                        resultSet.getString("City"),
+                        resultSet.getString("Country"),
+                        resultSet.getString("Phone")
+                );
+            }
 
-                    // while true loop to keep asking user for input until it is valid
-                    while (true) {
-                        System.out.println("\nWhich Category ID would you like to see to display all products?");
-                        System.out.print("Please choose between categories 1-8: ");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-                        if (theScanner.hasNextInt()) {
-                            categoryChoice = theScanner.nextInt();
+    }
 
-                            // if number is between 1-8
-                            if (categoryChoice >= 1 && categoryChoice <= 8) {
-                                // if user chooses valid input, exit the loop
-                                break;
-                            } else {
-                                // if user chooses invalid number, prints error message & goes back to start of loop to ask again
-                                System.out.println("Invalid Category ID. Please enter a number between 1-8.");
-                            }
-                        } else {
-                            // prints out error message if user enters in anything other than a number, like a string
-                            System.out.println("Invalid input. Please enter a number.");
-                            theScanner.next(); // clear invalid input
-                        }
-                    }
+    private static void displayAllCategories(Connection connection){
 
-                    // printing out products table and listing columns, included the CategoryID column as well for validation
-                    // try with resources ensures the prepared statement is automatically closed after use
-                    try (PreparedStatement preparedStatement = connection.prepareStatement(
-                            "SELECT Cat.CategoryID, Prod.ProductID, Prod.ProductName, Prod.UnitPrice, Prod.UnitsInStock " +
-                                    "FROM Products Prod " +
-                                    "JOIN Categories Cat ON Prod.CategoryID = Cat.CategoryID " +
-                                    "WHERE Prod.CategoryID = ?" )
-                    ) {
-                        // 1 is the placeholder for the question mark, then asking for the users choice
-                        preparedStatement.setInt(1, categoryChoice);
+        // try with resources ensures the prepared statement is automatically closed after use
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT CategoryID, CategoryName FROM Categories"
+        );
+             // execute query
+             ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            // header for categories list
+            System.out.println("\n            Categories: ");
+            System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
 
-                        // executing query and getting results
-                        // try with resources ensures the result set is automatically closed after use
-                        try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                            System.out.println("\nProduct Details from Category ID: " + categoryChoice);
-                            System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
+            // loop through the results
+            while (resultSet.next()) {
+                // process the data
+                System.out.printf(
+                        "Category ID:       %s\nCategory Name:     %s\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
+                        resultSet.getInt("CategoryID"),
+                        resultSet.getString("CategoryName")
+                );
 
-                            while (resultSet.next()) {
-                                System.out.printf(
-                                        "Category ID:   %s\nProduct ID:    %d\nProduct Name:  %s\nPrice:         %.2f\nStock:         %d\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
-                                        resultSet.getInt("CategoryID"),
-                                        resultSet.getInt("ProductID"),
-                                        resultSet.getString("ProductName"),
-                                        resultSet.getDouble("UnitPrice"),
-                                        resultSet.getInt("UnitsInStock")
-                                );
-                            }
+            }
 
-                        }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                    }
 
-                } else if (choice == 0) {
-                    // if user chooses 0
-                    // 0 = exit system, displays goodbye message
-                    System.out.println("────────────୨ৎ────────────");
-                    System.out.println("Exiting system now. Goodbye! •ᴗ•");
-                    running = false;
+    // passing scanner object as a parameter
+    private static void displayProductsByCategory(Connection connection, Scanner theScanner){
 
+        // initializing categoryChoice outside of loop
+        // using -1 as default vale to indicate it hasn't been assigned a valid category ID yet
+        int categoryChoice = -1;
+
+        // while true loop to keep asking user for input until it is valid
+        while (true) {
+            System.out.println("\nWhich Category ID would you like to see to display all products?");
+            System.out.print("Please choose between categories 1-8: ");
+
+            if (theScanner.hasNextInt()) {
+                categoryChoice = theScanner.nextInt();
+
+                // if number is between 1-8
+                if (categoryChoice >= 1 && categoryChoice <= 8) {
+                    // if user chooses valid input, exit the loop
+                    break;
                 } else {
-                    // if user chooses invalid number, print error message
-                    System.out.println("Invalid option, please choose between options 1, 2, 3, or 0.");
+                    // if user chooses invalid number, prints error message & goes back to start of loop to ask again
+                    System.out.println("Invalid Category ID. Please enter a number between 1-8.");
+                }
+            } else {
+                // prints out error message if user enters in anything other than a number, like a string
+                System.out.println("Invalid input. Please enter a number.");
+                theScanner.next(); // clear invalid input
+            }
+        }
+
+        // printing out products table and listing columns, included the CategoryID column as well for validation
+        // try with resources ensures the prepared statement is automatically closed after use
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT Cat.CategoryID, Prod.ProductID, Prod.ProductName, Prod.UnitPrice, Prod.UnitsInStock " +
+                        "FROM Products Prod " +
+                        "JOIN Categories Cat ON Prod.CategoryID = Cat.CategoryID " +
+                        "WHERE Prod.CategoryID = ?" )
+        ) {
+            // 1 is the placeholder for the question mark, then asking for the users choice
+            preparedStatement.setInt(1, categoryChoice);
+
+            // executing query and getting results
+            // try with resources ensures the result set is automatically closed after use
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                System.out.println("\nProduct Details from Category ID: " + categoryChoice);
+                System.out.println("─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────");
+
+                while (resultSet.next()) {
+                    System.out.printf(
+                            "Category ID:   %s\nProduct ID:    %d\nProduct Name:  %s\nPrice:         %.2f\nStock:         %d\n─────── ･ ｡ﾟ☆: *.☽ .*:☆ﾟ. ───────\n",
+                            resultSet.getInt("CategoryID"),
+                            resultSet.getInt("ProductID"),
+                            resultSet.getString("ProductName"),
+                            resultSet.getDouble("UnitPrice"),
+                            resultSet.getInt("UnitsInStock")
+                    );
                 }
 
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
     }
